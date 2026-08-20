eb42cb4e4b72503f704657edcc8f75e9118a2308
Peter Imini <peterimini2@gmail.com>
2026-08-20 21:57:59 +0100

    create automated test for formatCurrency

---
+++ tests/money-test.js
@@ -0,0 +1,25 @@
+import { formatCurrency } from "../js/utility/money.js"
+
+if (formatCurrency(2095) === '20.95') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+if (formatCurrency(0) === '0.00') {
+  console.log('passed');
+} else {
+  console.log('fail');
+}
+
+if (formatCurrency(2000.5) === '20.01') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+if (formatCurrency(2000.4) === '20.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
