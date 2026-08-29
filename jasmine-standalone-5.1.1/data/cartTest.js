d658a0ecc5d227f9fde936ea7640ebe94bd331f9
Peter Imini <peterimini2@gmail.com>
2026-08-29 12:03:11 +0100

    convert products into classes

--- jasmine-standalone-5.1.1/data/cartTest.js
+++ jasmine-standalone-5.1.1/data/cartTest.js
@@ -12,4 +12,4 @@ describe('test suite: addToCart', () => {
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart.length).toEqual(1);
   });
-});  
+});
--- js/checkout.js
+++ js/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "../js/checkout/order-summary.js";
 import { renderPaymentSummary } from "../js/checkout/payment-summary.js";
-import '../data-1/cart-class.js';
+// import '../data-1/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
