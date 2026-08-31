029951daea5c957ccb1a26d4f87c87e860c9896e
Peter Imini <peterimini2@gmail.com>
2026-08-31 20:47:24 +0100

    add backend practice file.

--- js/checkout.js
+++ js/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "../js/checkout/order-summary.js";
 import { renderPaymentSummary } from "../js/checkout/payment-summary.js";
 // import '../data-1/cart-class.js';
+import '../data-1/backend-practice.js';
 
 renderOrderSummary();
 renderPaymentSummary();
