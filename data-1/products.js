0ce1f810e1f1525e1e7cc036842da2e1fcf293b9
Peter Imini <peterimini2@gmail.com>
2026-09-01 11:07:21 +0100

    learn error handling

--- data-1/products.js
+++ data-1/products.js
@@ -101,10 +101,13 @@ export function loadProductsFetch() {
     });
 
     console.log('load products');
+  }).catch(() => {
+    console.log('Unexpected error. Please try again later.');
   })
 
   return promise;
 }
+
 /*
 loadProductsFetch().then(() => {
   console.log('next step');
@@ -125,6 +128,10 @@ export function loadProducts(fun) {
     console.log('load products');
 
     fun();
+  });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.')
   });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
--- js/checkout.js
+++ js/checkout.js
@@ -4,7 +4,30 @@ import { loadProducts, loadProductsFetch } from "../data-1/products.js";
 import { loadCart } from "../data-1/cart.js";
 // import '../data-1/cart-class.js';
 //import '../data-1/backend-practice.js';
+ 
+async function loadPage() {
+  try {
+    //throw 'error1';
+    
+    await loadProductsFetch();
 
+    const value = await new Promise((resolve, reject) => {
+      //throw 'error2';
+      loadCart(() => {
+       //reject('error3')
+        resolve('value3');
+      });
+    });
+  } catch(error) {
+    console.log('There is an error. Try again later.');
+  };
+
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
+
+/*
 Promise.all([
  loadProductsFetch(),
   new Promise((resolve) => {
@@ -18,6 +41,7 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 });
+*?
 
 /*
 new Promise((resolve) => {
